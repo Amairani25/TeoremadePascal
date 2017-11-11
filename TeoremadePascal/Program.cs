e60502270255b627d538c0259ded926ea54d9b86
amair <amair@LAPTOP-Q9P3M1OQ>
2017-11-10 23:12:01 -0600

    Add project files.

---
+++ TeoremadePascal/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeoremadePascal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Inserta el lado a ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserta el lado b ");
+            b = int.Parse(Console.ReadLine());
+            c =(a * a) + (b * b);
+            
+            Console.WriteLine(" La hipotenusa mide: " + Math.Sqrt(c));
+            Console.ReadKey();
+        }
+    }
+}
